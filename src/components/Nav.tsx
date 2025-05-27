d6ebf079a139574779b5f5d6b0b40f342ab1ee97
Emzzy241 <emzzyoluwole@gmail.com>
2025-05-27 14:58:09 +0100

    update Website navigation bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,18 @@
 "use client"; // If App Router is used, this must be a client component
 
-import link from "next/link";
-
 const Nav = () => {
     return (
-        <nav className="bg-gray-900 text-white p-4">
-            <ul className="flex space-x-4">
-                <li><link href="/">Home</link></li>
-                <li><link href="/about">About</link></li>
-                <li><link href="/contact">Contact</link></li>
+        <nav className="items-center bg-gray-900 text-white p-6 text-center flex space-x-8 rounded-full  ml-60 mr-60 mt-4">
+            <div className="logo ml-30">
+                {/* <img src="../../public/file.svg" alt="" /> */}
+                <p>AITTORNEY</p>
+            </div>
+            <ul className="flex space-x-10 text-center ml-40">
+                <li>How it Works</li>
+                <li>Resources</li>
+                <li>Pricing</li>
             </ul>
+            <button className="btn bg-[blue] p-1 rounded w-22 fs-10 border border-[#4f66d5] ml-40">Login</button>
         </nav>
     );
 }
