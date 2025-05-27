3b81444705bbedb0cb41e638f20fbb215709c607
Emzzy241 <emzzyoluwole@gmail.com>
2025-05-27 17:48:14 +0100

    add content section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 // import Image from "next/image";
 import Nav from "@/components/Nav";
 import HeroSection from "@/components/HeroSection";
+import ContentSection from "@/components/ContentSection";
+import ContentSection1 from "@/components/ContentSection1";
+import ContentSection2 from "@/components/ContentSection2";
 
 export default function Home() {
   return (
     <main className="flex min h-screen flex-col bg-[#141414]">
       <Nav />
       <HeroSection />
+      <ContentSection />
+      {/* core features component */}
+      <ContentSection1 />
+      {/* Submit your legal needs component */}
+      <ContentSection2 />
     </main>
   );
 }
---
+++ src/components/ContentSection1.tsx
@@ -0,0 +1,12 @@
+"use strict";
+
+const ContentSection1 = () => {
+    return (
+        <div className="text-left p-4 text-[#fff] border mt-7 mb-5 p-20">
+            <h4 className="rounded border p-2 bg-[#4d4e54] w-90">Get investor-ready legal docs in 3 simple steps</h4>
+            <h2 className="font-bold text-3xl mt-6">How it Works</h2>
+        </div>
+    );
+}
+
+export default ContentSection1;
