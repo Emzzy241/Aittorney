865abef096086b2b378cd378e9fcfe09c4520f80
Emzzy241 <emzzyoluwole@gmail.com>
2025-05-28 14:22:23 +0100

    reusing components with props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,26 @@
 import Nav from "@/components/Nav";
 import HeroSection from "@/components/HeroSection";
 import ContentSection from "@/components/ContentSection";
-import ContentSection1 from "@/components/ContentSection1";
-import ContentSection2 from "@/components/ContentSection2";
 
 export default function Home() {
   return (
     <main className="flex min h-screen flex-col bg-[#141414]">
       <Nav />
       <HeroSection />
-      <ContentSection />
+      <ContentSection
+        title="AI-Powered Legal Protection at Startup Speed"
+        subtitle="Aittorney for Startups & SMEs"
+      />
       {/* core features component */}
-      <ContentSection1 />
+      <ContentSection 
+        title="Get investor-ready legal docs in 3 simple steps"
+        subtitle="How it Works"
+      />
       {/* Submit your legal needs component */}
-      <ContentSection2 />
+      <ContentSection 
+        title="Legal Protection shouldn't break the bank"
+        subtitle="Simple Pricing for Startups & SMEs"
+      />
     </main>
   );
 }
